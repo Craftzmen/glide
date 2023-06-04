e1660c18ccb45c5f41a19afc363e771f7ce33737
Craftzmen <abdullahimran30.9e@gmail.com>
2023-06-04 22:59:13 +0500

    chore(branding): Updated logo and title

--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -1,14 +1,12 @@
 
 import React from 'react';
-import logo from './logo.png'
-
 
 
 export default function Navbar () {
     return (
         <div>
             <nav className={' flex justify-between items-center p-5 sm:px-20 lg:px-32 xl:px-52'}>
-                <img src={logo} alt="medflow"  className={' w-[120px] h-[25px] xl:w-[155px] xl:h-[35px] '}/>
+                <h1 className='text-teal-500 font-bold text-3xl' >Glide</h1>
                 <div className={' flex items-center gap-x-2'}>
                     <a href="/" className={' text-white/70 border border-white/70 hover:border-teal-500 hover:bg-teal-500 hover:text-teal-800 px-6 rounded-3xl py-2 '} > Login </a>
                     <a href="/" className={' bg-teal-500 text-teal-800 px-6 rounded-3xl py-2 '} > Register </a>
